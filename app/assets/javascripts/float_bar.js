f043846e89e8af6842dd3fd4e967a931bcbe191b
virgintech <ootani@virgintech.co.jp>
2015-12-12 21:39:48 +0900

    スティッキー・ナビ・バーの設置

---
+++ app/assets/javascripts/float_bar.js
@@ -0,0 +1,21 @@
+$(function(){
+  
+  $('.sticky-bar').each(function(){
+    
+    var $window=$(window),
+        $header=$(this),
+        headerOffsetTop=$header.offset().top;
+
+    $window.on('scroll',$.throttle(1000/15,function()
+    {
+      if($window.scrollTop()>headerOffsetTop){
+        $header.addClass('sticky');
+      }else {
+        $header.removeClass('sticky');
+      }
+    }));
+    
+    $window.trigger('scroll');
+    
+  });
+});
