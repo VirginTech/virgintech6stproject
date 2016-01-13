efba86c11ab09280de24550eb9184c964df18f2d
virgintech <ootani@virgintech.co.jp>
2016-01-13 16:40:03 +0900

    サイドバー位置修正、他微修正

--- app/assets/javascripts/side_bar.js
+++ app/assets/javascripts/side_bar.js
@@ -1,24 +1,27 @@
 $(function(){
 
   var duration=300;
-
+  var $window=$(window);
   var $aside=$('aside#category-nav');
   var $asideButton=$('.side-button').on('click',function(event){
       
       event.preventDefault();
       $aside.toggleClass('open');
-
+      
       if($aside.hasClass('open')){
+        //Topをスクロール位置に合わせる
+        $aside.css('top', $window.scrollTop() + 100);
+
         $aside.stop(true).animate({
           left:'-10px'
         },duration,'easeOutElastic');
-        $asideButton.text(" CLOSE")//文字変更
+        $asideButton.text(" CLOSE");//文字変更
         
       }else{
         $aside.stop(true).animate({
           left: -$aside.width()
         },duration,'easeOutElastic');
-        $asideButton.text(" OPEN")//文字変更
+        $asideButton.text(" OPEN");//文字変更
       }
     });
     
