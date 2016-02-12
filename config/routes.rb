8ecb77fc13b2c1a1f6f7810deec50c75e59e2d58
virgintech <ootani@virgintech.co.jp>
2016-02-12 20:36:52 +0900

    404エラーページ処理

--- config/routes.rb
+++ config/routes.rb
@@ -82,7 +82,10 @@ Rails.application.routes.draw do
 
   # 通知
   post 'notification', to: 'docs#notification'
-
+  
+  #エラーページ
+  get '*path', to: 'errors#render_404'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
