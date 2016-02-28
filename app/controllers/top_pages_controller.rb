724aa29c01e5825d76cd80276499cf59f0b010ad
virgintech <ootani@virgintech.co.jp>
2016-02-28 13:46:44 +0900

    動的スクロールバグ修正及びリザルトタイトル表示

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,5 +1,7 @@
 class TopPagesController < ApplicationController
   
+  include ApplicationHelper
+  
   def top
     @products=Product.all.order(created_at: :desc).limit(15)
     @user_comments = UserComment.all.order(created_at: :desc).limit(10)
@@ -22,9 +24,11 @@ class TopPagesController < ApplicationController
   def result
     # 新着順
     if params[:order]=="0"
+      @sort_title="新着順"
       @products=Product.order(created_at: :desc).page(params[:page]).per(20)
     # お気に入りランキング順
     elsif params[:order]=="1"
+      @sort_title="ランキング順"
       product_ids = Favorite.group(:product_id)
                             .order('count_product_id desc')
                             .count('product_id')
@@ -37,28 +41,36 @@ class TopPagesController < ApplicationController
       @products = Kaminari.paginate_array(products_).page(params[:page]).per(20)
     # カテゴリー
     elsif params[:order]=="2"
+      @sort_title=category_list.key(params[:category].to_i)
       @products = Product.where(category: params[:category]).order(created_at: :desc).page(params[:page]).per(20)
     # 機種
     elsif params[:order]=="3"
       if params[:model]=="1" # iPhone
+        @sort_title="iPhoneゲームアプリ"
         @products = Product.where(model_iphone: true).order(created_at: :desc).page(params[:page]).per(20)
-      elsif params[:model]=="2" # Android
+      elsif params[:model]=="3" # Android　なぜか「2番」を使うと2ページで終了してしまう。
+        @sort_title="Androidゲームアプリ"
         @products = Product.where(model_android: true).order(created_at: :desc).page(params[:page]).per(20)
-      elsif params[:model]=="3" # Web-Game
+      elsif params[:model]=="4" # Web-Game
+        @sort_title="Web(ブラウザ)ゲーム"
         @products = Product.where(model_web: true).order(created_at: :desc).page(params[:page]).per(20)
       end
     # 価格
     elsif params[:order]=="4"
+      @sort_title="#{price_list.key(params[:price].to_i)} アプリ"
       @products = Product.where(price: params[:price]).order(created_at: :desc).page(params[:page]).per(20)
     # セール
     elsif params[:order]=="5"
       if params[:sale]=="1" # リリース済み
+        @sort_title="リリース中"
         @products = Product.where('sale_date <= ?',Date.today).order(created_at: :desc).page(params[:page]).per(20)
       elsif params[:sale]=="2" # リリース待ち
+        @sort_title="リリース待ち"
         @products = Product.where('sale_date > ?',Date.today).order(created_at: :desc).page(params[:page]).per(20)
       end
     # 検索ボックス
     elsif params[:order]=="6"
+      @sort_title="「#{params[:search_word]}」の検索結果"
       @products=Product.where('appname like ?', "%#{params[:search_word]}%").order(created_at: :desc).page(params[:page]).per(20)
     end
 
